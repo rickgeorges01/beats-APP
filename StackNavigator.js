b2f198f8e7a1e581091b6c4e5847526278a6843d
rickgeorges <94584894+rickgeorges01@users.noreply.github.com>
2023-12-07 22:51:11 +0100

    Barre de navigation 90%

---
+++ StackNavigator.js
@@ -0,0 +1,79 @@
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import { Ionicons } from '@expo/vector-icons';
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+const Tab = createBottomTabNavigator();
+
+/**CONFIGURATION DE LA BARRE D'ONGLETS INFERIEURE**/
+function BottomTabs() {
+    return(
+        <Tab.Navigator screenOptions={{
+            tabBarStyle: {
+                backgroundColor: "rgba(0,0,0,0.5)",
+                position: "absolute",
+                bottom: 0,
+                left: 0,
+                right: 0,
+                shadowOpacity:4,
+                shadowRadius: 4,
+                elevation: 4,
+                shadowOffset: {
+                    width: 0,
+                    height: -4,
+                },
+                borderTopWidth:4
+            }
+        }}>
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: "Home",
+                    headerShown: false,
+                    tabBarLabelStyle: {color: 'white'},
+                    tabBarIcon:({focused}) => {
+                        focused? (
+                            <Ionicons name="ios-home" size={24} color="white" />
+                        ):(
+                            <Ionicons name="ios-home-outline" size={24} color="white" />
+                        )
+                    }
+                }}
+            />
+
+            <Tab.Screen
+                name="Profile"
+                component={ProfileScreen}
+                options={{
+                    tabBarLabel: "Home",
+                    headerShown: false,
+                    tabBarLabelStyle: {color: 'white'},
+                    tabBarIcon:({focused}) => {
+                        focused?(
+                            <Ionicons name="ios-person-circle" size={24} color="white" />
+                        ):(
+                            <Ionicons name="ios-person-circle-outline" size={24} color="white" />
+                        )
+                    }
+                }}
+            />
+        </Tab.Navigator>
+    )
+}
+
+const Stack = createNativeStackNavigator();
+function Navigation() {
+    return(
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Main" component={BottomTabs} options={{headerShown:false}}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+export default Navigation
+
+
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const ProfileScreen  = () => {
+  return (
+    <View>
+         <Text></Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ProfileScreen ;
